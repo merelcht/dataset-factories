d080aa59e356adf937ea7a683f65a7283251e6a2
Merel Theisen <merel.theisen@quantumblack.com>
2023-05-23 16:51:39 +0100

    Clean up repo
    Signed-off-by: Merel Theisen <merel.theisen@quantumblack.com>

--- src/dataset_factories/pipelines/data_processing/pipeline.py
+++ src/dataset_factories/pipelines/data_processing/pipeline.py
@@ -8,20 +8,23 @@ def create_pipeline(**kwargs) -> Pipeline:
         [
             node(
                 func=preprocess_companies,
-                inputs="companies",
-                outputs=["preprocessed_companies", "germany.companies@csv", "france.results@csv"],
+                inputs="companies@csv",
+                outputs="preprocessed_companies.02_intermediate@parquet",
                 name="preprocess_companies_node",
             ),
             node(
                 func=preprocess_shuttles,
                 inputs="shuttles",
-                outputs="preprocessed_shuttles",
+                outputs="preprocessed_shuttles.02_intermediate@parquet",
                 name="preprocess_shuttles_node",
             ),
             node(
                 func=create_model_input_table,
-                inputs=["preprocessed_shuttles", "preprocessed_companies", "reviews"],
-                outputs="model_input_table",
+                inputs=[
+                    "preprocessed_shuttles.02_intermediate@parquet",
+                    "preprocessed_companies.02_intermediate@parquet",
+                    "reviews@csv"],
+                outputs="model_input_table.03_primary@parquet",
                 name="create_model_input_table_node",
             ),
         ]
